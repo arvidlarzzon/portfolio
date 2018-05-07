27b69c15a5c2345d3e6ae67e04f9b7d681ace032
arvidlarzzon <arvidlarzzon@hotmail.com>
2018-05-07 12:55:52 +0200

    pic fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import profilePic from '../Assets/Jag.jpg'
 import '../Styles/Header.css'
 
 class Header extends Component {
@@ -16,7 +17,7 @@ class Header extends Component {
     return (
         <div className="App-header">
           <div className="header-wrapper">
-            <img src='public/jag.jpg' className="profileImg" alt="logo" />
+            <img src={profilePic} className="profileImg" alt="logo" />
             <span className="name-span">Arvid Larsson</span>
             {this.props.children /*This is the menu*/}
           </div>
