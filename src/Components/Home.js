32a97dc87d9062a51c7b2c820c9fded7b7f9abba
arvidlarzzon <arvidlarzzon@hotmail.com>
2018-05-07 15:07:02 +0200

    new favicon and title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,10 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
+      <div className="home">
         <h1>This is Home component</h1>
+        <p>Här har jag lite genvägar med en thumbnail till projects, about eller resumé</p>
+      </div>
     );
   }
 }
